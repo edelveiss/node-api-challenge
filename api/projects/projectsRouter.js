8f099681a6ef521dc404edcd25c697200c010989
Tatiana Zhizhimontova <tatyana.zhi@gmail.com>
2020-07-17 18:16:50 -0700

    dependancies are installed, router files are created

---
+++ api/projects/projectsRouter.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const Projects = require("../../data/helpers/projectModel.js");
+const Actions = require("../../data/helpers/actionModel.js");
+const router = express.Router();
+
+module.exports = router;
